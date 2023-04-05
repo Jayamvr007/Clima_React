168b12ae0a7b728c8f57515726adb4884ece9669
Jayam Verma <jayamverma@gmail.com>
2023-04-05 12:10:09 +0530

    First commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "34c06930542d747fde7bd19b5dbd5d05",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
